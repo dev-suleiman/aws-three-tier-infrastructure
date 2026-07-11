65f197967249b13c3085e252e1db25fc527e14d4
Suleiman <bmdanjumah@gmail.com>
2026-07-11 20:19:11 +0000

    feat(vpc): add multi-AZ VPC with three-tier subnets

---
+++ modules/vpc/output.tf
@@ -0,0 +1,34 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.main.id
+}
+
+output "vpc_cidr" {
+  description = "VPC CIDR block"
+  value       = aws_vpc.main.cidr_block
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+}
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+}
+
+output "database_subnet_ids" {
+  description = "List of database subnet IDs"
+  value       = [aws_subnet.database_1.id, aws_subnet.database_2.id]
+}
+
+output "nat_gateway_id" {
+  description = "ID of the NAT Gateway"
+  value       = aws_nat_gateway.main.id
+}
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = aws_internet_gateway.main.id
+}
